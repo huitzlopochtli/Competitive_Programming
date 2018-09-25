ace5016515e05a3d796c8ec8543e04e7e24127e5
Khondokar Hashibul Islam Peeyal (PK) <peeyalk@hotmail.com>
2018-09-25 15:16:08 +0600

    Solved -- Minimum Swaps 2 with mergesort

--- HankerRank/Interview Preparation Kit/Array/Minimum Swaps 2.cpp
+++ HankerRank/Interview Preparation Kit/Array/Minimum Swaps 2.cpp
@@ -6,47 +6,74 @@ void printArray(int arr[], int n)
         std::cout << arr[i] <<" ";
 }
 
-int partition(int a[],int l,int u)
+void merge(int arr[], int l, int m, int r)
 {
-    int v,i,j,temp;
-    v=a[l];
-    i=l;
-    j=u+1;
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
 
-    do
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
     {
-        do
-            i++;
-
-        while(a[i]<v&&i<=u);
-
-        do
-            j--;
-        while(v<a[j]);
-
-        if(i<j)
+        if (L[i] <= R[j])
         {
-            temp=a[i];
-            a[i]=a[j];
-            a[j]=temp;
+            arr[k] = L[i];
+            i++;
         }
-    }while(i<j);
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
 
-    a[l]=a[j];
-    a[j]=v;
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
 
-    return(j);
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
 }
 
-
-void quick_sort(int a[],int l,int u)
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
 {
-    int j;
-    if(l<u)
+    if (l < r)
     {
-        j=partition(a,l,u);
-        quick_sort(a,l,j-1);
-        quick_sort(a,j+1,u);
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l+(r-l)/2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+
+        merge(arr, l, m, r);
     }
 }
 
@@ -65,7 +92,7 @@ int main()
         arrCopy[i] = arr[i];
     }
 
-    quick_sort(arrCopy,0, length-1);
+    mergeSort(arrCopy,0, length-1);
 
     int minSwap = 0;
 
